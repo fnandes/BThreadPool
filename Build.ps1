fd8795b563ddc4c1ddbba11f812aa6f42f85c0be
Pedro Fernandes | Stone <pfernandes@stone.com.br>
2017-05-09 18:08:20 -0300

    First commit

---
+++ Build.ps1
@@ -0,0 +1,29 @@
+if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+
+# Evaluate next version based on AppVeyor build version
+$actual_version = "$env:APPVEYOR_BUILD_VERSION"
+Write-Host "Set version to $actual_version"
+
+# Set version on project files
+ls */*/project.json | foreach { echo $_.FullName} |
+foreach {
+    $content = get-content "$_"
+    $content = $content.Replace("99.99.99", $actual_version)
+    set-content "$_" $content -encoding UTF8
+}
+
+& dotnet restore
+if ($LASTEXITCODE -ne 0)
+{
+    throw "dotnet restore failed with exit code $LASTEXITCODE"
+}
+
+& dotnet pack .\src\BThreadPool -c Release -o .\artifacts
+
+# Rollback version on project files
+ls */*/project.json | foreach { echo $_.FullName} |
+foreach {
+    $content = get-content "$_"
+    $content = $content.Replace($actual_version, "99.99.99")
+    set-content "$_" $content -encoding UTF8
+}
